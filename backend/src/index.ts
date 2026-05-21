d7c8f09bdc3e9f15d7b0247faa5a3d7bd2c6fab6
enzo vallejos <enzonahuelvallejos@gmail.com>
2026-05-21 19:41:20 -0300

    ejemplo de front

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import guiaRouter from "./routes/guia";
 import ejemploRouter from "./routes/ejemplo";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(express.json());
 
